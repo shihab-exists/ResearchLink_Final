4b68121ca8d01f6a2ead260936103218fe57b2d7
Shihab <mdshiblirahmanshihab@Mds-MacBook-Air.local>
2026-08-05 11:14:28 +0600

    Fixed build.sh to explicitly install requirements

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
-
-echo "🚀 Running Render Build commands..."
-
-# Run migrations
-echo "🗄️ Running Django Database Migrations..."
+pip install -r requirements.txt
 python manage.py migrate
-
-# Collect static files
-echo "🎨 Collecting Static Files using WhiteNoise..."
 python manage.py collectstatic --no-input
-
-echo "✅ Render Build Completed Successfully!"
